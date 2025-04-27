e16b44459aeb2d89a70ad1e36f5ed047d6ac02e4
林振凯 <97156089+zhenkai01@users.noreply.github.com>
2025-04-27 11:33:25 +0800

    Update test5.py

--- test5.py
+++ test5.py
@@ -2,6 +2,4 @@ keys = ['name', 'age', 'city']
 values = ['Alice', 25, 'New york']
 dictionary = dict(zip(keys, values))
 print(dictionary)
-# print("第一部分")
-# print("第二部分")
-# print("第三部分")
+print("第二部分")
