0ebea687fb781022254292fc5851b33b91973541
林振凯 <97156089+zhenkai01@users.noreply.github.com>
2025-05-15 15:31:01 +0800

    Update test5.py

--- test5.py
+++ test5.py
@@ -2,6 +2,9 @@ keys = ['name', 'age', 'city']
 values = ['Alice', 25, 'New york']
 dictionary = dict(zip(keys, values))
 print(dictionary)
+def function():
+    # 开发了一半的代码
+
 print("What is amend  !")
 print("第一部分")
 print("第二部分")
